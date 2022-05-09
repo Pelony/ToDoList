720b765fc72499495315e994fa209f6653df15e6
Pelony <61895704+Pelony@users.noreply.github.com>
2022-05-08 19:09:26 -0500

    Agregacion de fecha

--- js/view.js
+++ js/view.js
@@ -72,26 +72,51 @@ export default class View {
     this.model.removeTodo(id);
     document.getElementById(id).remove();
   }
-
+  
   createRow(todo) {
+    const fecha=new Date();
+    const months = ["JAN", "FEB", "MAR","APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+  
+  const formatDate = (fecha)=>{
+      let formatted_date = fecha.getDate() + "-" + months[fecha.getMonth()] + "-" + fecha.getFullYear()
+      return formatted_date;
+  }
+      console.log(formatDate(fecha));
+    const valor=document.getElementById('fecha');
+    document.getElementById('fecha').innerHTML='Fecha: '+formatDate(fecha);
+
+    //table
     const row = table.insertRow();
     row.setAttribute('id', todo.id);
     row.innerHTML = `
       <td>${todo.title}</td>
       <td>${todo.description}</td>
+      <td> </td>
       <td class="text-center">
 
       </td>
       <td class="text-right">
 
       </td>
+  
     `;
+    const fecha_text=document.createElement('p');
+    fecha_text.type='text';
+    fecha_text.innerHTML=''+formatDate(fecha);
+    row.children[2].appendChild(fecha_text);
+    
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.checked = todo.completed;
     checkbox.onclick = () => this.toggleCompleted(todo.id);
-    row.children[2].appendChild(checkbox);
+    row.children[3].appendChild(checkbox);
+
+    const boton_fecha=document.createElement('button');
+    boton_fecha.classList.add('btn','btn-success','mb-1');
+    boton_fecha.innerHTML = '<i class="fa-solid fa-calendar"></i>';
+    boton_fecha.onclick=()=> alert("Aun no sirve el boton xd");
+    row.children[4].appendChild(boton_fecha);
 
     const editBtn = document.createElement('button');
     editBtn.classList.add('btn', 'btn-primary', 'mb-1');
@@ -104,12 +129,14 @@ export default class View {
       description: row.children[1].innerText,
       completed: row.children[2].children[0].checked,
     });
-    row.children[3].appendChild(editBtn);
+    row.children[4].appendChild(editBtn);
 
     const removeBtn = document.createElement('button');
     removeBtn.classList.add('btn', 'btn-danger', 'mb-1', 'ml-1');
     removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
     removeBtn.onclick = () => this.removeTodo(todo.id);
-    row.children[3].appendChild(removeBtn);
+    row.children[4].appendChild(removeBtn);
+
+    
   }
 }
